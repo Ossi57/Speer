806572de3acdae849428cdbb6754ad08ddef0c4d
Ossi57 <oguzhan.ermis@hotmail.com>
2017-12-20 19:31:23 +0100

    Letzer Stand Prototyp: Fertig - TdoT präsentiert. Interface begonnen.

--- WpfApplication1/Einlagern.xaml.cs
+++ WpfApplication1/Einlagern.xaml.cs
@@ -18,13 +18,11 @@ using System.Data.SqlClient;
 
 namespace WpfApplication1
 {
-    /// <summary>
+    /// <summary>x
     /// Interaktionslogik für Window1.xaml
     /// </summary>
     public partial class Window1 : MetroWindow
     {
-        static int[] array = Enumerable.Range(1, 100).ToArray();
-
         public int currentbestand;
         public int artikelid;
         public string artikelname;
@@ -34,13 +32,12 @@ namespace WpfApplication1
         static CultureInfo ci = new CultureInfo("de-DE");
         static SpeechRecognitionEngine sre = new SpeechRecognitionEngine(ci);
 
-        public Dictionary<string, int> textNumber;
-
+        MainWindow mainwindow = new MainWindow();
 
         public Window1(int currentbestand, int artikelid, string artikelname)
         {
             InitializeComponent();
-            Form1_Load1();
+            mainwindow.Form1_Load1();            
 
             if (i2 != 0)
             {
@@ -66,111 +63,6 @@ namespace WpfApplication1
             //MessageBox.Show(currentbestand +" , "+artikelid+" , "+artikelname);
         }
 
-    private void Form1_Load1()
-        {
-            textNumber = new Dictionary<string, int>();
-            textNumber.Add("eins", 1);
-            textNumber.Add("zwei", 2);
-            textNumber.Add("drei", 3);
-            textNumber.Add("vier", 4);
-            textNumber.Add("fünf", 5);
-            textNumber.Add("sechs", 6);
-            textNumber.Add("sieben", 7);
-            textNumber.Add("acht", 8);
-            textNumber.Add("neun", 9);
-            textNumber.Add("zehn", 10);
-            textNumber.Add("elf", 11);
-            textNumber.Add("zwölf", 12);
-            textNumber.Add("dreizehn", 13);
-            textNumber.Add("vierzehn", 14);
-            textNumber.Add("fünfzehn", 15);
-            textNumber.Add("sechszehn", 16);
-            textNumber.Add("siebzehn", 17);
-            textNumber.Add("achtzehn", 18);
-            textNumber.Add("neunzehn", 19);
-            textNumber.Add("zwanzig", 20);
-            textNumber.Add("einundzwanzig", 21);
-            textNumber.Add("zweiundzwanzig", 22);
-            textNumber.Add("dreiundzwanzig", 23);
-            textNumber.Add("vierundzwanzig", 24);
-            textNumber.Add("fünfundzwanzig", 25);
-            textNumber.Add("sechsundzwanzig", 26);
-            textNumber.Add("siebenundzwanzig", 27);
-            textNumber.Add("achtundzwanzig", 28);
-            textNumber.Add("neunundzwanzig", 29);
-            textNumber.Add("dreißig", 30);
-            textNumber.Add("einunddreißig", 31);
-            textNumber.Add("zweiunddreißig", 32);
-            textNumber.Add("dreiunddreißig", 33);
-            textNumber.Add("vierunddreißig", 34);
-            textNumber.Add("fünfunddreißig", 35);
-            textNumber.Add("sechsunddreißig", 36);
-            textNumber.Add("siebenunddreißig", 37);
-            textNumber.Add("achtunddreißig", 38);
-            textNumber.Add("neununddreißig", 39);
-            textNumber.Add("vierzig", 40);
-            textNumber.Add("einundvierzig", 41);
-            textNumber.Add("zweiundvierzig", 42);
-            textNumber.Add("dreiundvierzig", 43);
-            textNumber.Add("vierundvierzig", 44);
-            textNumber.Add("fünfundvierzig", 45);
-            textNumber.Add("sechsundvierzig", 46);
-            textNumber.Add("siebenundvierzig", 47);
-            textNumber.Add("achtundvierzig", 48);
-            textNumber.Add("neunundvierzig", 49);
-            textNumber.Add("fünfzig", 50);
-            textNumber.Add("einundfünfzig", 51);
-            textNumber.Add("zweiundfünfzig", 52);
-            textNumber.Add("dreiundfünfzig", 53);
-            textNumber.Add("viersundfünfzig", 54);
-            textNumber.Add("Fünfundfünfzig", 55);
-            textNumber.Add("sechsundfünfzig", 56);
-            textNumber.Add("siebenundfünfzig", 57);
-            textNumber.Add("achtundfünfzig", 58);
-            textNumber.Add("neunundfünfzig", 59);
-            textNumber.Add("sechzig", 60);
-            textNumber.Add("einundsechzig", 61);
-            textNumber.Add("zweiundsechzig", 62);
-            textNumber.Add("dreiundsechzig", 63);
-            textNumber.Add("viersundsechzig", 64);
-            textNumber.Add("fünfundsechzig", 65);
-            textNumber.Add("sechsundsechzig", 66);
-            textNumber.Add("siebenundsechzig", 67);
-            textNumber.Add("achtundsechzig", 68);
-            textNumber.Add("neunundsechzig", 69);
-            textNumber.Add("siebzig", 70);
-            textNumber.Add("einundsiebzig", 71);
-            textNumber.Add("zweiundsiebzig", 72);
-            textNumber.Add("dreiundsiebzig", 73);
-            textNumber.Add("vierundsiebzig", 74);
-            textNumber.Add("fünfundsiebzig", 75);
-            textNumber.Add("sechsundsiebzig", 76);
-            textNumber.Add("siebenundsiebzig", 77);
-            textNumber.Add("achtundsiebzig", 78);
-            textNumber.Add("neunundsiebzig", 79);
-            textNumber.Add("achtzig", 80);
-            textNumber.Add("einundachtzig", 81);
-            textNumber.Add("zweiundachtzig", 82);
-            textNumber.Add("dreiundachtzig", 83);
-            textNumber.Add("vierundachtzig", 84);
-            textNumber.Add("fünfundachtzig", 85);
-            textNumber.Add("sechsundachtzig", 86);
-            textNumber.Add("siebenundachtzig", 87);
-            textNumber.Add("achtundachtzig", 88);
-            textNumber.Add("neunundachtzig", 89);
-            textNumber.Add("Neunzig", 90);
-            textNumber.Add("einundneunzig", 91);
-            textNumber.Add("Zweiundneunzig", 92);
-            textNumber.Add("dreiundneunzig", 93);
-            textNumber.Add("vierundneunzig", 94);
-            textNumber.Add("fünfundneunzig", 95);
-            textNumber.Add("sechsundneunzig", 96);
-            textNumber.Add("siebenundneunzig", 97);
-            textNumber.Add("achtundneunzig", 98);
-            textNumber.Add("neunundneunzig", 99);
-            textNumber.Add("hundert", 100);
-        }
-
         static Grammar GetHelloGoodbyeGrammar()
         {
             Choices ch_HelloGoodbye = new Choices();
@@ -185,7 +77,7 @@ namespace WpfApplication1
         public Grammar getStückzahl()
         {
             Choices ch_Stck = new Choices();
-            foreach (KeyValuePair<string, int> pair in textNumber)
+            foreach (KeyValuePair<string, int> pair in mainwindow.textNumber)
             {
                 ch_Stck.Add(pair.Key.ToString());
             }
@@ -193,25 +85,6 @@ namespace WpfApplication1
             Grammar g_result = new Grammar(gb_result);
             return g_result;
         }
-
-        private void getValues()
-        {
-           
-        }
-
-        /*
-        private void checkBox2_CheckedChanged(object sender, EventArgs e)
-        {
-            
-            if (checkBox2.Checked == true)
-                sre.RecognizeAsync(RecognizeMode.Multiple);
-            else if (checkBox2.Checked == false) // Turn off
-                sre.RecognizeAsyncCancel();
-                
-        }
-        */
-
-
 
         public void sre_SpeechRecognized(object sender,
           SpeechRecognizedEventArgs e)
@@ -222,9 +95,9 @@ namespace WpfApplication1
             float conf = e.Result.Confidence;
             if (conf < 0.65) return;
 
-            if (textNumber.ContainsKey(txt))
+            if (mainwindow.textNumber.ContainsKey(txt))
             {
-                int value = textNumber[txt];
+                int value = mainwindow.textNumber[txt];
                 ValueS = value;
 
                 this.Dispatcher.Invoke(() =>
@@ -251,7 +124,6 @@ namespace WpfApplication1
                     string Query = "update Artikel set bestand='" + counter + "' where id='" + artikelid + "' ";
                     SqlCommand createCommand = new SqlCommand(Query, con);
                     createCommand.ExecuteNonQuery();
-                    //MessageBox.Show("abcd");
                     con.Close();
                 }
                 catch (Exception ex)
--- WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/MainWindow.xaml.cs
@@ -29,7 +29,6 @@ namespace WpfApplication1
     {
 
         //initalize SpeechRecognizer
-        static int[] array = Enumerable.Range(1, 100).ToArray();
         static CultureInfo ci = new CultureInfo("de-DE");
         static SpeechRecognitionEngine sre = new SpeechRecognitionEngine(ci);
 
@@ -39,8 +38,12 @@ namespace WpfApplication1
         public int artikelid { get; set; }
         public string artikelname { get; set; }
 
+        public Dictionary<string, int> textNumber;
+
         public MainWindow()
         {
+
+
             InitializeComponent();
             LoadDataGrid();
             sre.SetInputToDefaultAudioDevice();
@@ -51,19 +54,118 @@ namespace WpfApplication1
                                            
         }
 
+        public void Form1_Load1()
+        {
+            textNumber = new Dictionary<string, int>();
+            textNumber.Add("eins", 1);
+            textNumber.Add("zwei", 2);
+            textNumber.Add("drei", 3);
+            textNumber.Add("vier", 4);
+            textNumber.Add("fünf", 5);
+            textNumber.Add("sechs", 6);
+            textNumber.Add("sieben", 7);
+            textNumber.Add("acht", 8);
+            textNumber.Add("neun", 9);
+            textNumber.Add("zehn", 10);
+            textNumber.Add("elf", 11);
+            textNumber.Add("zwölf", 12);
+            textNumber.Add("dreizehn", 13);
+            textNumber.Add("vierzehn", 14);
+            textNumber.Add("fünfzehn", 15);
+            textNumber.Add("sechszehn", 16);
+            textNumber.Add("siebzehn", 17);
+            textNumber.Add("achtzehn", 18);
+            textNumber.Add("neunzehn", 19);
+            textNumber.Add("zwanzig", 20);
+            textNumber.Add("einundzwanzig", 21);
+            textNumber.Add("zweiundzwanzig", 22);
+            textNumber.Add("dreiundzwanzig", 23);
+            textNumber.Add("vierundzwanzig", 24);
+            textNumber.Add("fünfundzwanzig", 25);
+            textNumber.Add("sechsundzwanzig", 26);
+            textNumber.Add("siebenundzwanzig", 27);
+            textNumber.Add("achtundzwanzig", 28);
+            textNumber.Add("neunundzwanzig", 29);
+            textNumber.Add("dreißig", 30);
+            textNumber.Add("einunddreißig", 31);
+            textNumber.Add("zweiunddreißig", 32);
+            textNumber.Add("dreiunddreißig", 33);
+            textNumber.Add("vierunddreißig", 34);
+            textNumber.Add("fünfunddreißig", 35);
+            textNumber.Add("sechsunddreißig", 36);
+            textNumber.Add("siebenunddreißig", 37);
+            textNumber.Add("achtunddreißig", 38);
+            textNumber.Add("neununddreißig", 39);
+            textNumber.Add("vierzig", 40);
+            textNumber.Add("einundvierzig", 41);
+            textNumber.Add("zweiundvierzig", 42);
+            textNumber.Add("dreiundvierzig", 43);
+            textNumber.Add("vierundvierzig", 44);
+            textNumber.Add("fünfundvierzig", 45);
+            textNumber.Add("sechsundvierzig", 46);
+            textNumber.Add("siebenundvierzig", 47);
+            textNumber.Add("achtundvierzig", 48);
+            textNumber.Add("neunundvierzig", 49);
+            textNumber.Add("fünfzig", 50);
+            textNumber.Add("einundfünfzig", 51);
+            textNumber.Add("zweiundfünfzig", 52);
+            textNumber.Add("dreiundfünfzig", 53);
+            textNumber.Add("viersundfünfzig", 54);
+            textNumber.Add("Fünfundfünfzig", 55);
+            textNumber.Add("sechsundfünfzig", 56);
+            textNumber.Add("siebenundfünfzig", 57);
+            textNumber.Add("achtundfünfzig", 58);
+            textNumber.Add("neunundfünfzig", 59);
+            textNumber.Add("sechzig", 60);
+            textNumber.Add("einundsechzig", 61);
+            textNumber.Add("zweiundsechzig", 62);
+            textNumber.Add("dreiundsechzig", 63);
+            textNumber.Add("viersundsechzig", 64);
+            textNumber.Add("fünfundsechzig", 65);
+            textNumber.Add("sechsundsechzig", 66);
+            textNumber.Add("siebenundsechzig", 67);
+            textNumber.Add("achtundsechzig", 68);
+            textNumber.Add("neunundsechzig", 69);
+            textNumber.Add("siebzig", 70);
+            textNumber.Add("einundsiebzig", 71);
+            textNumber.Add("zweiundsiebzig", 72);
+            textNumber.Add("dreiundsiebzig", 73);
+            textNumber.Add("vierundsiebzig", 74);
+            textNumber.Add("fünfundsiebzig", 75);
+            textNumber.Add("sechsundsiebzig", 76);
+            textNumber.Add("siebenundsiebzig", 77);
+            textNumber.Add("achtundsiebzig", 78);
+            textNumber.Add("neunundsiebzig", 79);
+            textNumber.Add("achtzig", 80);
+            textNumber.Add("einundachtzig", 81);
+            textNumber.Add("zweiundachtzig", 82);
+            textNumber.Add("dreiundachtzig", 83);
+            textNumber.Add("vierundachtzig", 84);
+            textNumber.Add("fünfundachtzig", 85);
+            textNumber.Add("sechsundachtzig", 86);
+            textNumber.Add("siebenundachtzig", 87);
+            textNumber.Add("achtundachtzig", 88);
+            textNumber.Add("neunundachtzig", 89);
+            textNumber.Add("Neunzig", 90);
+            textNumber.Add("einundneunzig", 91);
+            textNumber.Add("Zweiundneunzig", 92);
+            textNumber.Add("dreiundneunzig", 93);
+            textNumber.Add("vierundneunzig", 94);
+            textNumber.Add("fünfundneunzig", 95);
+            textNumber.Add("sechsundneunzig", 96);
+            textNumber.Add("siebenundneunzig", 97);
+            textNumber.Add("achtundneunzig", 98);
+            textNumber.Add("neunundneunzig", 99);
+            textNumber.Add("hundert", 100);
+        }
 
         public void LoadDataGrid()
         {
             
-            String path = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-                
-            
+            //String path = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            //MessageBox.Show(path);      
 
-
-            MessageBox.Show(path);
-
-
-            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename= C:\Users\hakan\Source\Repos\Speer\WpfApplication1\LagerDB.mdf;Integrated Security=True");
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Oguzhan\Documents\GitHub\Speer\WpfApplication1\LagerDB.mdf;Integrated Security=True");
             SqlDataAdapter Query = new SqlDataAdapter(@"SELECT Id, artikelname, bestand FROM Artikel", con);
             DataTable dt = new DataTable();
 
@@ -71,16 +173,6 @@ namespace WpfApplication1
 
             dataGrid1.ItemsSource = null;
             dataGrid1.ItemsSource = dt.DefaultView;
-
-            /*
-             * OLD DataGrid
-            LagerDBEntities1 dataEntities = new LagerDBEntities1();
-            var query =
-                from product in dataEntities.Artikel
-                select new { product.Id, product.artikelname, product.bestand };
-            dataGrid1.ItemsSource = null;
-            dataGrid1.ItemsSource = query.ToList();
-            */
         }
 
         static Grammar GetHelloGoodbyeGrammar()
@@ -93,18 +185,6 @@ namespace WpfApplication1
             Grammar g_result = new Grammar(gb_result);
             return g_result;
         }
-
-        /*
-        private void checkBox2_CheckedChanged(object sender, EventArgs e)
-        {
-            
-            if (checkBox2.Checked == true)
-                sre.RecognizeAsync(RecognizeMode.Multiple);
-            else if (checkBox2.Checked == false) // Turn off
-                sre.RecognizeAsyncCancel();
-                
-        }
-        */
 
         void sre_SpeechRecognized(object sender,
           SpeechRecognizedEventArgs e)
@@ -163,30 +243,6 @@ namespace WpfApplication1
         {
             selected = true;
 
-            /*
-             *OLD SELECTION 
-            var dg = sender as DataGrid;
-            if (dg == null) return;
-            var index = dg.SelectedIndex;
-
-            //here we get the actual row at selected index
-            DataGridRow row = dg.ItemContainerGenerator.ContainerFromIndex(index) as DataGridRow;
-
-            //here we get the actual data item behind the selected row
-
-            var item = dg.ItemContainerGenerator.ItemFromContainer(row).ToString().Trim(new char[] { ' ' });
-
-            string[] meineStrings = item.ToString().Split(new Char[] { ',', '=', '}' });
-
-
-            MessageBox.Show(item.ToString());
-
-            artikelid = Int32.Parse(meineStrings[-1]);
-            artikelname = meineStrings[3];
-            currbestand = Int32.Parse(meineStrings[5]);
-            */
-
-
             DataRowView oDataRowView = dataGrid1.SelectedItem as DataRowView;
             string sValue = string.Empty;
 
@@ -196,18 +252,6 @@ namespace WpfApplication1
                 artikelname = oDataRowView.Row["artikelname"] as string;
                 currbestand = (int)oDataRowView.Row["bestand"];
             }
-
-            /*
-             * TEST
-            MessageBox.Show(artikelid.ToString());
-            MessageBox.Show(artikelname);
-            MessageBox.Show(currbestand.ToString());*/
-
-
         }
     }
 }
-
-
-    
-
